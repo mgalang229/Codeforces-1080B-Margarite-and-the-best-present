acd11584f1a33f34c5e2a256628abdf737c1771e
Jason Galang <51401355+mgalang229@users.noreply.github.com>
2021-10-03 17:13:15 +0800

    Update sol.cpp

--- sol.cpp
+++ sol.cpp
@@ -13,7 +13,7 @@ int main() {
 		--l; // we need to decrement the value of 'l' in order to find the sum between 'l' and 'r'
 		// view the image in this repository for the derivation of formula
 		// if we perform the operation in every i-th number, then we can see that for every number
-		// the sum at that point will be ('i' + 1) / 2 (if it's even) and ('i' + 1) / 2 * -1 (if it's odd
+		// the sum at that point will be ('i' + 1) / 2 (if it's even) and ('i' + 1) / 2 * -1 (if it's odd)
 		// to get the sum between the interval of 'l' and 'r' (inclusive), then calculate their difference:
 		// = sum at point 'r' - sum at point 'l - 1'
 		long long left = (l % 2 == 0 ? (l + 1) / 2 : (l + 1) / 2 * (-1));
